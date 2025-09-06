6d87e810d197f8ce00db5d8a15cec53fd70761ff
Sefat Ullah Fahad <mdf780709@gmail.com>
2025-09-06 19:50:32 +0600

    adedd a sound section

--- script/script.js
+++ script/script.js
@@ -1,4 +1,17 @@
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
+
+
+
+
+
+
+
 const createElement = (arr)=> {
     const htmlElements = arr.map(el => `<span class="btn">${el}</span>`)
     return (htmlElements.join(""))
@@ -131,7 +144,7 @@ const displayLevelWord = (words) => {
             <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'} / ${word.pronunciation ? word.pronunciation : 'pronunciation পাওয়া যায়নি'}"</div>
             <div class="flex justify-between items-center">
                 <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1a90ff22] hover:bg-[#1a90ff7f]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1a90ff22] hover:bg-[#1a90ff7f]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1a90ff22] hover:bg-[#1a90ff7f]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
         </div>
         `
